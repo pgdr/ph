278dc5e3e1e0c8e89dc4bea621b084b46b3c4253
Pål Grønås Drange <pgdr@equinor.com>
2020-03-09 18:47:08 +0100

    Fix setup file

--- setup.py
+++ setup.py
@@ -1,29 +1,56 @@
-import setuptools
+#!/usr/bin/env python3
+
+from setuptools import setup
+
 import os
 
+__pgdr = "PG Drange <pgdr@equinor.com>"
+__source = "https://github.com/pgdr/sh"
+__webpage = __source
+__description = "ph - the tabular data shell tool"
 
-def _here(fname):
-    return os.path.join(os.path.dirname(__file__), fname)
-
-
-README = ""
-with open(_here("README.md"), "r") as fin:
-    README = "".join(fin.readlines())
-
-REQUIREMENTS = []
-with open(_here("requirements.txt"), "r") as fin:
-    REQUIREMENTS = [x.strip() for x in fin.readlines()]
+requirements = {
+    "minimum": ["pandas"],
+}
 
 
-setuptools.setup(
+def src(x):
+    root = os.path.dirname(__file__)
+    return os.path.abspath(os.path.join(root, x))
+
+
+def _read_file(fname, op):
+    with open(src(fname), "r") as fin:
+        return op(fin.readlines())
+
+
+def readme():
+    try:
+        return _read_file("README.md", lambda lines: "".join(lines))
+    except:
+        return __description
+
+
+setup(
+    version="0.0.3",
     name="ph",
-    version="0.0.1",
-    author="Pål Grønås Drange",
-    license="MIT",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pgdr/ph",
-    entry_points={"console_scripts": ["ph=ph:main"]},
     packages=["ph"],
-    install_requires=REQUIREMENTS,
+    description=__description,
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="PG Drange",
+    author_email="pgdr@equinor.com",
+    maintainer=__pgdr,
+    url=__webpage,
+    project_urls={
+        "Bug Tracker": "{}/issues".format(__source),
+        "Documentation": "{}/blob/master/README.md".format(__source),
+        "Source Code": __source,
+    },
+    license="MIT",
+    keywords="tabular data, pandas, csv, pipeline, unix, command line tool",
+    install_requires=requirements["minimum"],
+    entry_points={"console_scripts": ["ph = ph:main",],},
+    test_suite="tests",
+    extras_require=requirements,
 )
