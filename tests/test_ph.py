fa550351155fa7f2611446599860ddc7ce70319b
Pål Grønås Drange <PGDR@equinor.com>
2020-03-31 10:46:27 +0200

    Remove duplicate test and fix bug in open

--- tests/test_ph.py
+++ tests/test_ph.py
@@ -86,21 +86,16 @@ def test_sep_from(phmgr):
     assert list(captured.df.shape) == [6, 3]
 
 
-def test_sep_to(capsys, monkeypatch):
+def test_sep_to_sub(capsys, monkeypatch):
     monkeypatch.setattr("sys.stdin", _get_io("d"))
     ph.COMMANDS["to"]("csv", sep="_")
     captured = Capture(capsys.readouterr())
-
     assert not captured.err
-    assert list(captured.df.shape) == [6, 3]
+    assert list(captured.df.shape) == [6, 1]
 
-
-def test_sep_to(capsys, monkeypatch):
-    monkeypatch.setattr("sys.stdin", _get_io("d", extension="scsv"))
-    ph.COMMANDS["from"]("csv", sep=";")
-    captured = Capture(capsys.readouterr())
-    assert not captured.err
-    assert list(captured.df.shape) == [6, 3]
+    df = pd.read_csv(io.StringIO(captured.out), sep="_")
+    assert list(df.shape) == [6, 4]
+    assert list(df["year"]) == list(range(2003, 2009))
 
 
 def test_thousands_from(capsys, monkeypatch):
